a6f6c4ec9f7b670d51828048b08816f905a25326
s1250091 <s1250091@ilab1dc44.u-aizu.ac.jp>
2018-07-05 19:22:03 +0900

    Won/lost message added

--- Ex3.c
+++ Ex3.c
@@ -4,17 +4,18 @@
 
 int main(){
   srand((unsigned int)time(NULL));
-  int die1,die2;
+  int die1,die2,total;
   printf("Rolling the dice...\n");
 
   die1=rand()%6+1;
   die2=rand()%6+1;
   printf("Die 1: %d\n",die1);
 
-    printf("Die 1: %d\n",die2);
-
-    printf("Total value: %d\n",die1+die2);
-
+    printf("Die 2: %d\n",die2);
+    total=die1+die2;
+    printf("Total value: %d\n",total);
+    if(total>7)printf("You won!\n");
+    else printf("You lose\n");
     return 0;
 
 
