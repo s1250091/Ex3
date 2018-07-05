e9fada56abf6f102d776888d6e1dde3314a34ada
s1250091 <s1250091@ilab1dc44.u-aizu.ac.jp>
2018-07-05 19:18:36 +0900

    Greetings added

--- Ex3.c
+++ Ex3.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 int main(){
-  srand((unsigned int)time(NULL));
-  int die1,die2;
-  printf("Rolling the dice...\n");
 
-  die1=rand()%6+1;
-  die2=rand()%6+1;
-  printf("Die 1: %d\n",die1);
+  char name[10];
 
-    printf("Die 1: %d\n",die2);
+  printf("What is your name?\n>");
 
-    printf("Total value: %d\n",die1+die2);
+  scanf("%s",name);
 
-    return 0;
+  printf("Hello,%s!\n",name);
 
+  return 0;
 
 }
+  
